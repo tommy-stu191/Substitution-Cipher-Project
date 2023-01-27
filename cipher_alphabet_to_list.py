65c6b6b12f23bb641b06261ef7406d09395be9e0
josh-thomp12 <123507792+josh-thomp12@users.noreply.github.com>
2023-01-27 13:23:44 -0600

    commented recommendations

---
+++ cipher_alphabet_to_list.py
@@ -0,0 +1,9 @@
+# Needs a docstring
+# For consistency and conciseness among named variables,
+# consider changing string >> cipher_alphabet
+
+def cipher_alphabet_list(string):
+    cipher_list = []
+    for char in string:
+        cipher_list.append(char)
+    return cipher_list
--- cipher_alphabet_tolist.py
+++
@@ -1,5 +0,0 @@
-def cipher_alphabet_list(string):
-    cipher_list = []
-    for char in string:
-        cipher_list.append(char)
-    return cipher_list
