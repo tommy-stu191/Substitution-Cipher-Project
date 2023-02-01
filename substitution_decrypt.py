819928f856bdfec8e16fcbdbb06c0a604b8020c5
josh-thomp12 <123507792+josh-thomp12@users.noreply.github.com>
2023-02-01 12:10:53 -0600

    Add files via upload
    A very simple solution to the base decrypt function

---
+++ substitution_decrypt.py
@@ -0,0 +1,13 @@
+def substitution_decrypt(ciphertext_file, plaintext_file, cipher_alphabet):
+    """
+
+    :param ciphertext_file: The *.txt file being decrypted
+    :param plaintext_file: The name of the *.txt file returned
+    :param cipher_alphabet: A string "key" for the encryption
+    :return: A *.txt plaintext file
+    """
+    return substitution_encrypt(ciphertext_file, plaintext_file, cipher_alphabet)
+
+
+
+
