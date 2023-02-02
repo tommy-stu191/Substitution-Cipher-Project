8bb62e9e9bb487791e8927584aed3ebf719f37bb
bvuncan <123507952+bvuncan@users.noreply.github.com>
2023-02-02 15:22:50 -0600

    Add files via upload

--- a_inverse.py
+++ a_inverse.py
@@ -1,27 +1,8 @@
+def a_inverse(a):
+    for int in range(0, 26):
+        if a*int % 26 == 1:
+            return int
 
-def affine_alph(a,b):
-    """
-
-    :param a: integer for affine cipher
-    :param b: integer for affine cipher
-    :return: the affine alphabet for encryption
-    """
-    alpha_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                   'u', 'v', 'w', 'x', 'y', 'z']
-    count = 0
-    affine_cipher = []
-
-    while len(affine_cipher) != 26: # loop until the affine cipher alphabet is full
-        for char in alpha_lower:
-            alpha_index = alpha_lower.index(char) # find the index of the current character for calculation
-            if (a*alpha_index+b)%26 == count: # to ensure the cipher
-                affine_cipher.append(char)
-                count += 1
-    affine_alphabet = ''.join(affine_cipher)
-    return affine_alphabet
-
-
-print(affine_alph(9,7))
 
 
 
---
+++ affine_alph.py
@@ -0,0 +1,20 @@
+def affine_alph(a,b):
+    """
+
+    :param a: integer for affine cipher
+    :param b: integer for affine cipher
+    :return: the affine alphabet for encryption
+    """
+    alpha_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                   'u', 'v', 'w', 'x', 'y', 'z']
+    count = 0
+    affine_cipher = []
+
+    while len(affine_cipher) != 26: # loop until the affine cipher alphabet is full
+        for char in alpha_lower:
+            alpha_index = alpha_lower.index(char) # find the index of the current character for calculation
+            if (a*alpha_index+b)%26 == count: # to ensure the cipher
+                affine_cipher.append(char)
+                count += 1
+    affine_alphabet = ''.join(affine_cipher)
+    return affine_alphabet
