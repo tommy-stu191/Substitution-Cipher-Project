f5709aa7032859c799503904e0557c649aa11648
bvuncan <123507952+bvuncan@users.noreply.github.com>
2023-02-02 14:45:59 -0600

    Add files via upload

--- a_inverse.py
+++ a_inverse.py
@@ -16,18 +16,16 @@ def affine_alph(a,b):
     count = 0
     affine_cipher = []
 
-    for char in alpha_lower:
-        alpha_index = alpha_lower.index(char)
-        print(count)
-        print((a*alpha_index+b)%26) #grab the index of the current letter being checked
-        if (a*alpha_index+b)%26 == count:
-            affine_cipher = affine_cipher.append(char)
-            print(affine_cipher)
-            alpha_lower.remove(char)
-            count += 1
+    while len(affine_cipher) != 26: # loop until the affine cipher alphabet is full
+        for char in alpha_lower:
+            alpha_index = alpha_lower.index(char) # find the index of the current character for calculation
+            if (a*alpha_index+b)%26 == count: # to ensure the cipher
+                affine_cipher.append(char)
+                print(affine_cipher)
+                count += 1
 
 
-print(affine_alph(3,7))
+affine_alph(9,7)
 
 
 
