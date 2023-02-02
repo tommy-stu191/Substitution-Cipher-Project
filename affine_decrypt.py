915a3a41bf8c7ca55b55360aa9b75411e7c17771
bvuncan <123507952+bvuncan@users.noreply.github.com>
2023-02-02 15:30:11 -0600

    Add files via upload

--- affine_decrypt.py
+++ affine_decrypt.py
@@ -1,2 +1,4 @@
 def affine_decrypt(ciphertext_file, plaintext_file, a, b):
-    return affine_encrypt(ciphertext_file, plaintext_file, a, b)
+    cipher_string = affine_alph_decrypt(a,b)
+    cipher = substitution_encrypt(ciphertext_file, plaintext_file, cipher_string)
+    return cipher
