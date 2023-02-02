4db836b20cb88f861827f9b258bb899395d821d1
bvuncan <123507952+bvuncan@users.noreply.github.com>
2023-02-02 15:31:44 -0600

    Add files via upload

--- affine_decrypt.py
+++ affine_decrypt.py
@@ -1,4 +1,8 @@
 def affine_decrypt(ciphertext_file, plaintext_file, a, b):
+    if not ab_checker(a,b):
+        print("Invalid cipher_alphabet")
+        exit()
+
     cipher_string = affine_alph_decrypt(a,b)
     cipher = substitution_encrypt(ciphertext_file, plaintext_file, cipher_string)
     return cipher
